b8b4fac4aa413fd604eb97cc429bfa7ecef9bcc8
Cies Breijs <cies@kde.nl>
2011-05-23 22:29:35 +0200

    added todo's, added yuksels quran txt
    tanzil txt now clean, added word counts

--- count.rb
+++ count.rb
@@ -12,40 +12,41 @@
 require 'awesome_print'
 
 # This so we no not have to deal with unicode in the rest of the code.
+# Got it from: http://www.al-bab.com/arab/language/roman1.htm
 ALPHABET = {
   alif: 'ا',
   ba:   'ب',
-  ta1:  'ت',
-  ta2:  'ث',
-  gim:  'ج',
-  ha1:  'ح',
-  ha2:  'خ',
-  dal1: 'د',
-  dal2: 'ذ',
+  ta:   'ت',
+  tha:  'ث',
+  jim:  'ج',
+  ha:   'ح',
+  hha:  'خ',
+  dal:  'د',
+  dhal: 'ذ',
   ra:   'ر',
   zay:  'ز',
   sin:  'س',
   shin: 'ش',
   sad:  'ص',
   dad:  'ض',
-  ta3:  'ط',
-  za:   'ظ',
+  tah:  'ط',
+  zah:  'ظ',
   ayn:  'ع',
-  gayn: 'غ',
+  ghayn:'غ',
   fa:   'ف',
   qaf:  'ق',
   kaf:  'ك',
   lam:  'ل',
   mim:  'م',
   nun:  'ن',
-  ha3:  'ه',
+  ha:   'ه',
   waw:  'و',
   ya:   'ي'
 }
 
 
 # Array of arab letters that occur infront of suras.
-#  [:alif, :lam, :mim, :ra, :sad, :ha1, :ta3, :sin, :ha3, :ya, :ayn, :qaf, :nun, :kaf],
+#  [:alif, :lam, :mim, :ra, :sad, :ha, :tah, :sin, :ha, :ya, :ayn, :qaf, :nun, :kaf],
 
 
 # Fill up a hash (sura_nr => sura_txt) from the quran file as supplied.
@@ -53,12 +54,12 @@ ALPHABET = {
 #   <sura_nr>|<aya_nr>|<aya_text>
 # i.e.:
 #   19|19|قَالَ إِنَّمَا أَنَا رَسُولُ رَبِّكِ لِأَهَبَ لَكِ غُلَامًا زَكِيًّا
-quran = {}
+@@quran = {}
 open(ARGV[0]).each_line do |l|
   if l.count('|') == 2
     sura, aya, txt = l.split('|')
     sura = sura.to_i
-    quran[sura] ? quran[sura] += txt : quran[sura] = txt
+    @@quran[sura] ? @@quran[sura] += txt : @@quran[sura] = txt
   end
 end
 
@@ -69,14 +70,14 @@ LETTERS_AND_SURAS = {
   [:alif, :lam, :mim, :sad] => [7],
   [:alif, :lam, :ra] => (10..15).to_a - [13],
   [:alif, :lam, :mim, :ra] => [13],
-  [:kaf, :ha3, :ya, :ayn, :sad] => [19],
-  [:ta3, :ha3] => [20],
-  [:ta3, :sin, :mim] => [26, 28],
-  [:ta3, :sin] => [27],
+  [:kaf, :ha, :ya, :ayn, :sad] => [19],
+  [:tah, :ha] => [20],
+  [:tah, :sin, :mim] => [26, 28],
+  [:tah, :sin] => [27],
   [:ya, :sin] => [36],
   [:sad] => [38],
-  [:ha1, :mim] => (40..46).to_a - [42],
-  [:ha1, :mim, :ayn, :sin, :qaf] => [42],
+  [:ha, :mim] => (40..46).to_a - [42],
+  [:ha, :mim, :ayn, :sin, :qaf] => [42],
   [:qaf] => [50],
   [:nun] => [68]
 }
@@ -90,9 +91,13 @@ letter_counts.each_key { |k| letter_counts[k] = Array.new(k.length, 0) }
 LETTERS_AND_SURAS.each_pair do |letters, suras|
   letters.each_with_index do |letter, i|
     suras.each do |sura|
-      letter_counts[letters][i] += quran[sura].scan(ALPHABET[letter]).size
+      letter_counts[letters][i] += @@quran[sura].scan(ALPHABET[letter]).size
     end
   end
+end
+
+def with_mod19(number)
+  "#{number} (mod19: #{number%19})"
 end
 
 # Calculate totals, and print them with mod19s to the stdout.
@@ -100,12 +105,36 @@ letter_counts.each_pair do |letters,counts|
   total = counts.inject(0) { |sum,i| sum += i }
   print "#{letters.to_s.ljust(30)} "
   print "#{ counts.to_s.ljust(30)} => "
-  print "#{total} (mod19: #{total%19})\n"
+  print "#{with_mod19(total)}\n"
 end
 
+puts "\n"
 
-allah_count = 0
-quran.each_value do |sura_txt|
-  allah_count += sura_txt.scan("اللَّه").size
+def count_string(str, latinization = nil)
+  count = 0
+  @@quran.each_value { |sura_txt| count += sura_txt.scan(str).size }
+  puts "Total occurences #{("'" + (latinization || str) + "':").ljust(10)} #{with_mod19(count)}"
 end
-puts "\n\n\nTotal occurences of the word 'Allah': #{allah_count} (mod19: #{allah_count%19})"
+
+def count_letters_in_sura(sura_nr)
+  sura_txt = @@quran[sura_nr]
+  sura_txt.gsub!("بسم الله الرحمن الرحيم", '')
+  total = 0
+  istr = ''
+  sura_txt[1..-1].gsub(/\s+/,' ').split(/\s+/).each_with_index do |word,i|
+    puts "#{i} #{word}: #{word.length}"
+    total += word.length
+    istr << word.length.to_s
+  end
+  puts istr
+  #sura_txt.gsub!(/\s+/, '')
+  puts "The number of letters in sura #{sura_nr}: #{with_mod19(total)}"
+end
+
+count_string "بسم", "Bism"
+count_string "الله", "Allah"
+count_string "رحمن", "Rahman"
+count_string "رحيم", "Rahim"
+count_string "ا", "Alif"
+
+count_letters_in_sura 96
